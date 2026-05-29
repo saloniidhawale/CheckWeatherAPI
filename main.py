b66412e28a5387bf283df578f39d502905cb35ea
dhawalesaloni <salonidhawale@gmail.com>
2026-05-29 01:20:32 -0400

    debugging API key errors

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("hello")
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+API_KEY = "YOUR_API_KEY_HERE"
+
+@app.route("/")
+def home():
+
+    city = "Toronto"
+
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+    response = requests.get(url, timeout=5)
+
+    print("STATUS CODE:", response.status_code)
+    print("TEXT RESPONSE:", response.text)
+
+    return response.text
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
